1a7bf501db90a0090bd42eb60660b27939cd3af9
Michal Fojtik <mfojtik@redhat.com>
2024-03-24 16:34:08 +0100

    initial metrics reporter

--- go.mod
+++ go.mod
@@ -2,10 +2,21 @@ module github.com/mfojtik/go-dht-prometheus
 
 go 1.20
 
-require github.com/d2r2/go-dht v0.0.0-20200119175940-4ba96621a218
+require (
+	github.com/d2r2/go-dht v0.0.0-20200119175940-4ba96621a218
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/prometheus/client_golang v1.19.0
+)
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22 // indirect
 	github.com/d2r2/go-shell v0.0.0-20211022052110-f591c27e3e2e // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,26 +1,103 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/d2r2/go-dht"
+	"github.com/jessevdk/go-flags"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func main() {
-	// Read DHT11 sensor data from pin 4, retrying 10 times in case of failure.
-	// You may enable "boost GPIO performance" parameter, if your device is old
-	// as Raspberry PI 1 (this will require root privileges). You can switch off
-	// "boost GPIO performance" parameter for old devices, but it may increase
-	// retry attempts. Play with this parameter.
-	// Note: "boost GPIO performance" parameter is not work anymore from some
-	// specific Go release. Never put true value here.
-	temperature, humidity, retried, err :=
-		dht.ReadDHTxxWithRetry(dht.DHT22, 4, false, 10)
-	if err != nil {
-		log.Fatal(err)
+var (
+	lastTemperatureGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "last_temperature",
+		Help: "Last measured temperature by DHT sensor",
+	})
+	lastHumidityGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "last_humidity",
+		Help: "Last measured humidity by DHT sensor",
+	})
+	last_successful_measurement_seconds = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "last_successful_measurement_seconds",
+		Help: "Number of seconds that passed from the last successfully measurement",
+	})
+	last_measurement_retries = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "last_measurement_retries",
+		Help: "Number of retries by DHT sensor since it got values",
+	})
+)
+
+var opts struct {
+	sensorType       int           `long:"sensor-type" default:"3"`
+	sensorPIN        int           `long:"sensor-pin" default:"4"`
+	sensorMaxRetries int           `long:"sensor-max-retries" default:"5"`
+	listenAddr       string        `long:"listen-addr" required:"true" default:":2112"`
+	readSeconds      time.Duration `long:"interval" default:"5"`
+}
+
+func recordMetrics() {
+	last_measurement_time := time.Now()
+	for {
+		temperature, humidity, retried, err := dht.ReadDHTxxWithRetry(
+			dht.SensorType(opts.sensorType),
+			opts.sensorPIN,
+			false,
+			opts.sensorMaxRetries,
+		)
+		if err != nil {
+			log.Printf("ERROR: DHT sensor reported: %v", err)
+		}
+
+		log.Printf("DHT: %.2 C, %.2%%", temperature, humidity)
+
+		// record amount of seconds since the last successful measurement
+		last_successful_measurement_seconds.Set(float64(time.Now().Unix() - last_measurement_time.Unix()))
+		last_measurement_time = time.Now()
+		lastTemperatureGauge.Set(float64(temperature))
+		lastHumidityGauge.Set(float64(humidity))
+		last_measurement_retries.Set(float64(retried))
+
+		time.Sleep(opts.readSeconds)
 	}
-	// Print temperature and humidity
-	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-		temperature, humidity, retried)
+}
+
+func main() {
+	if _, err := flags.Parse(&opts); err != nil {
+		log.Fatalf("ERR: %v", err)
+	}
+
+	server := &http.Server{
+		Addr: opts.listenAddr,
+	}
+
+	go recordMetrics()
+	http.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+		log.Println("Stopped serving new connections.")
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	log.Println("Graceful shutdown complete.")
 }
